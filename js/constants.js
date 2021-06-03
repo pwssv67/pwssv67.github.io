cb912e1f78d1cca097930461526a4ceac282277a
pwssv67 <61920177+pwssv67@users.noreply.github.com>
2021-06-03 13:11:25 +0300

    translation to russian added

---
+++ js/constants.js
@@ -0,0 +1,47 @@
+const navElementsTitlesEng = ["Main", "About", "Skills", "Projects", "Contact"]
+const navElementsTitlesRus = ["Главная", "Обо мне", "Навыки", "Проекты", "Контакты"]
+const nameEng = "Egor Smolyaninov";
+const nameRus = "Егор Смольянинов";
+
+const aboutEng = "Junior Android Developer <br> 21 year old, Saint-Petersburg";
+const aboutRus = "Разработчик мобильных приложений. <br> 21 год. Санкт-Петербург";
+
+const factsEng = [
+        "Hi! I'm Egor, Android Dev from Saint-Petersburg<br>I'm interested in Mobile development and everything connected with it",
+        "I'm studying at \"Information Systems and Technologies\"<br> in SPbSUAI",
+        "Ready to implement excellent projects<br>with interesting people"
+];
+const factsRus = [
+    "Привет! Я Егор, Android-разработчик из Санкт-Петербурга. <br>Интересуюсь мобильной разработкой и всем, что с ней связано.",
+    "Учусь на специальности \"Информационные системы и технологии\" <br>в СПбГУАП.",
+    "Готов воплощать в жизнь смелые и интересные проекты<br>с хорошими людьми."
+];
+
+const skillsDescriptionEng = "I work with such technologies as";
+const skillsDescriptionRus = "Я работаю с такими технологиями как";
+
+const contactDescriptionEng = "Want to know more or just chat?<br>You are welcome!";
+const contactDescriptionRus = "Хочешь узнать больше или просто поговорить? <br>You are welcome!";
+
+const projectDescriptionsEng = [
+    "Fitness app with a unique algorithm of exercise recognition via accelerometer, providing you ability to compete with another users nearby or worldwide, or you can create custom group of friends to show them your metal.<br><br>" +
+     "Developed with Koin, Android Navigation Component, Firebase, Kotlin Coroutines.",
+     "App for makeup and manicure masters, to fill out all the necessary forms and consents from clients in electronic form, with the ability to either send the form to the client by email for filling out, or fill it out directly on the device, after which the form is saved in pdf for printing.<br><br>"+
+     "Developed with Dagger2, RxJava2, Retrofit.",
+     "Small app to count amount of water you drink, calories you eat and time of daily exercise. You can also see your statistics, set your goals and read articles about fitness.<br><br>"+
+     "Developed with Jetpack Compose, Retrofit, Room."
+];
+const projectDescriptionsRus = [
+    "Фитнес-приложение с уникальныи алгоритмом распознавания пяти видов упражнений с помощью акселерометра, с возможностью соревноваться с другими людьми как по всему миру, так и в самостоятельно созданной группе или среди ближайших людей.<br><br>" +
+     "Приложение разработано с использованием Koin, Android Navigation Components, Firebase, Kotlin Coroutines.",
+     "Приложение для мастеров макияжа и маникюра для заполнения всех необходимых форм и согласий от клиентов в электронном виде, с возможностью как отправить форму клиенту на почту для заполнения, так и заполнить прямо на устройстве, после чего форма сохраняется в pdf для печати.<br><br>"+
+     "Разработано с использованием Dagger2, RxJava2, Retrofit.",
+     "Приложение чтобы следить за количеством выпитой воды, употреблённых калорий и времени физической нагрузки каждый день, а также посмотреть свою статистику, настроить свои цели и просмотреть статьи, связанные со здоровым образом жизни.<br><br>"+
+     "Разработано с использованием Jetpack Compose, Retrofit, Room."
+];
+
+const contactEng = "Contact";
+const contactRus = "Связаться";
+
+const contactMeEng = "Contact me on<br>LinkedIn, Instagram, Telegram";
+const contactMeRus = "Свяжись со мной в<br>LinkedIn, Instagram, Telegram";
--- js/main.js
+++ js/main.js
@@ -39,19 +39,28 @@ function setEnglishLanguage() {
 function setRusLanguage() {
     document.getElementsByClassName("eng")[0].classList.remove("chosed");
     document.getElementsByClassName("ru")[0].classList.add("chosed");
+    translateToRussian();
 }
 
 function translateToEnglish() {
-    translateNavigationToEnglish();
-    translateHeaderToEnglish();
-    translateAboutToEnglish();
-    translateDescriptionsToEnglish();
-    translateProjectsToEnglish();
-    translateContactsToEnglish();
+    translateNavigation(navElementsTitlesEng);
+    translateHeader(nameEng, aboutEng);
+    translateFacts(factsEng);
+    translateDescriptions(skillsDescriptionEng, contactDescriptionEng);
+    translateProjects(projectDescriptionsEng);
+    translateContacts(contactEng, contactMeEng);
 }
 
-function translateNavigationToEnglish() {
-    let navElementsTitles = ["Main", "About", "Skills", "Projects", "Contact"];
+function translateToRussian() {
+    translateNavigation(navElementsTitlesRus);
+    translateHeader(nameRus, aboutRus);
+    translateFacts(factsRus);
+    translateDescriptions(skillsDescriptionRus, contactDescriptionRus);
+    translateProjects(projectDescriptionsRus);
+    translateContacts(contactRus, contactMeRus);
+}
+
+function translateNavigation(navElementsTitles) {
     let navElements = document.getElementsByClassName("nav-item");
     for (let i = 0; i < navElements.length; i++) {
         navElements[i].textContent = navElementsTitles[i];
@@ -63,54 +72,32 @@ function translateNavigationToEnglish() {
     }
 }
 
-function translateHeaderToEnglish() {
-    let name = "Egor Smolyaninov";
+function translateHeader(name, about) {
     document.getElementsByClassName("name")[0].textContent = name;
-
-    let about = "Junior Android Developer <br> 21 year old, Saint-Petersburg";
     document.getElementsByClassName("short-about")[0].innerHTML = about;
 }
 
 
-function translateAboutToEnglish() {
-    let title = "About";
-    let facts = [
-        "Hi! I'm Egor, Android Dev from Saint-Petersburg\nI'm interested in Mobile development and everything connected with it",
-        "I'm studying at \"Information Systems and Technologies\"\n in SPbSUAI",
-        "Ready to implement excellent projects\nwith interesting people"
-    ]
+function translateFacts(facts) {
     let factElements = document.getElementsByClassName("fact")
     for (let i = 0; i<factElements.length;i++) {
-        factElements[i].textContent = facts[i];
+        factElements[i].innerHTML = facts[i];
     }
 }
 
-function translateDescriptionsToEnglish() {
-    let skillsDescription = "I work with such technologies as";
-    let contactDescription = "Want to know more or just chat?<br>You are welcome!"
+function translateDescriptions(skillsDescription, contactDescription) {
     document.getElementsByClassName("description")[0].textContent = skillsDescription;
     document.getElementsByClassName("description")[1].innerHTML = contactDescription;
 }
 
-function translateProjectsToEnglish() {
-    let projectDescriptions = [
-        "Fitness app with a unique algorithm of exercise recognition via accelerometer, providing you ability to compete with another users nearby or worldwide, or you can create custom group of friends to show them your metal.<br>" +
-         "Developed with Koin, Android Navigation Component, Firebase, Kotlin Coroutines.",
-         "App for makeup and manicure masters, to fill out all the necessary forms and consents from clients in electronic form, with the ability to either send the form to the client by email for filling out, or fill it out directly on the device, after which the form is saved in pdf for printing.<br>"+
-         "Developed with Dagger2, RxJava2, Retrofit.",
-         "Small app to count amount of water you drink, calories you eat and time of daily exercise. You can also see your statistics, set your goals and read articles about fitness.<br>"+
-         "Developed with Jetpack Compose, Retrofit, Room."
-    ];
-
+function translateProjects(projectDescriptions) {
     let projectDescriptionElements = document.getElementsByClassName("project-description");
     for (let i = 0; i<projectDescriptionElements.length; i++) {
         projectDescriptionElements[i].innerHTML = projectDescriptions[i];
     }
 }
 
-function translateContactsToEnglish() {
-    let contact = "Contact";
-    let contactMe = "Contact me on<br>LinkedIn, Instagram, Telegram";
+function translateContacts(contact, contactMe) {
     document.getElementsByClassName("email")[0].textContent = contact;
     document.getElementsByClassName("last")[0].innerHTML = contactMe;
 }
